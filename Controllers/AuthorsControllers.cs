18f9db520b9ae026a30591be6bcf1d38310da4c9
Ali Ahmed <ali.ahmed@punchcard.io>
2020-03-23 23:32:25 -0600

    First Commit - Creating Controllers, using AutoMapper, using ActionResult

---
+++ Controllers/AuthorsControllers.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("/api/authors")]
+    public class AuthorsControllers: ControllerBase 
+    {
+        private readonly ICourseLibraryRepository _clRepo;
+        private readonly IMapper _mapper;
+
+        public AuthorsControllers(ICourseLibraryRepository clRepo, IMapper mapper)
+        {
+            _clRepo = clRepo ?? 
+                throw new ArgumentException(nameof(clRepo));
+            _mapper = mapper ??
+                throw new ArgumentException(nameof(mapper));
+        }
+
+        [HttpGet("/api/authors")]
+        public ActionResult<IEnumerable<AuthorDTO>> GetAuthors()
+        {
+            var authors = _clRepo.GetAuthors();
+
+            var result = _mapper.Map<IEnumerable<AuthorDTO>>(authors);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{authorId:guid}")]
+        public ActionResult<AuthorDTO> GetAuthor(Guid authorId)
+        {
+            var author = _clRepo.GetAuthor(authorId);
+            if (author == null)
+                return NotFound();
+                
+            return new JsonResult(_mapper.Map<AuthorDTO>(author));
+        }
+    }
+}
---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors/{authorId}/courses")]
+    public class CoursesController: ControllerBase
+    {
+        private readonly ICourseLibraryRepository _clRepo;
+        private readonly IMapper _mapper;
+
+        public CoursesController(ICourseLibraryRepository clRepo, IMapper mapper)
+        {
+            _clRepo = clRepo ??
+                throw new ArgumentException(nameof(clRepo));
+           _mapper = mapper ??
+                throw new ArgumentException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CourseDTO>> GetCoursesFromAuthor(Guid authorId)
+        {
+            if (!_clRepo.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courses = _clRepo.GetCourses(authorId);
+            var result = _mapper.Map<IEnumerable<CourseDTO>>(courses);
+            return Ok(result);
+        }
+
+        [HttpGet("/{courseId}")]
+        public ActionResult<CourseDTO> GetCourseForAuthor(Guid authorId, Guid courseId)
+        {
+            if (!_clRepo.AuthorExists(authorId))
+                return NotFound();
+
+            var course = _clRepo.GetCourse(authorId, courseId);
+
+            if (course == null)
+                return NotFound();
+
+            var result = _mapper.Map<CourseDTO>(course);
+
+            return Ok(result);
+        }
+    }
+}
